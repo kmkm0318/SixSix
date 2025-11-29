eee42f4393f35032824331070fd27f1e3886da48
kmkm0318 <kmkm0318@naver.com>
2025-11-29 15:24:46 +0900

    상호작용 UI가 목표를 따라가도록 변경

--- Assets/Scripts/UI/InteractionInfoUI/InteractionInfoUI.cs
+++ Assets/Scripts/UI/InteractionInfoUI/InteractionInfoUI.cs
@@ -1,19 +1,56 @@
 using UnityEngine;
 
+
 /// <summary>
 /// 다이스 오브젝트 및 갬블 다이스 아이콘 UI의 상호작용 정보를 보여주는 UI
 /// </summary>
+[RequireComponent(typeof(RectTransform))]
 public class InteractionInfoUI : MonoBehaviour
 {
-    [SerializeField] private AnimatedText _infoText;
-    [SerializeField] private DiceInteractionTypeDataList _interactionTypeDatas;
+    [SerializeField] private AnimatedText _infoText; //정보 텍스트 오브젝트
+    [SerializeField] private DiceInteractionTypeDataList _interactionTypeDatas; //상호작용 타입 데이터 리스트
+    [SerializeField] private float _offset = 25f; //UI 위치 오프셋
 
-    private Transform _target;
+    private Camera _mainCamera; //메인 카메라
+    private RectTransform _rectTransform; //자신의 RectTransform
+    private Transform _target; //대상 트랜스폼
+    private float _targetOffset; //대상 오프셋
+    private bool _isUI = false; //대상이 UI인지 여부
 
     private void Start()
     {
         RegisterEvents();
+        _mainCamera = Camera.main;
+        _rectTransform = GetComponent<RectTransform>();
         gameObject.SetActive(false);
+    }
+
+    private void LateUpdate()
+    {
+        HandlePosition();
+    }
+
+    // UI 위치 처리
+    private void HandlePosition()
+    {
+        if (_target == null) return;
+
+        Vector3 targetPos = _target.position;
+
+        if (_isUI)
+        {
+            //대상이 UI인 경우 그대로 오프셋 적용
+            targetPos += new Vector3(0f, _targetOffset, 0f);
+        }
+        else
+        {
+            //대상이 월드 오브젝트인 경우 카메라 좌표로 변환하기 전 오프셋 적용
+            targetPos = _mainCamera.WorldToScreenPoint(_target.position + new Vector3(0f, _targetOffset, 0f));
+        }
+
+        //공통 오프셋 적용
+        targetPos.y += _offset;
+        _rectTransform.position = targetPos;
     }
 
     private void OnDestroy()
@@ -36,6 +73,17 @@ public class InteractionInfoUI : MonoBehaviour
     private void OnShowInteractionInfoUI(Transform target, DiceInteractionType type, int value)
     {
         _target = target;
+
+        if (target is RectTransform rect)
+        {
+            _isUI = true;
+            _targetOffset = rect.rect.height / 2f;
+        }
+        else
+        {
+            _isUI = false;
+            _targetOffset = target.lossyScale.y / 2f;
+        }
 
         if (_interactionTypeDatas.DataDict.TryGetValue(type, out var data))
         {
