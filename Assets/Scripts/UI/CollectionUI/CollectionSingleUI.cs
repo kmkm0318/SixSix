b8209b7d3faabcbe324f3cc444a55c96b42a5840
kmkm0318 <kmkm0318@naver.com>
2025-12-15 14:43:25 +0900

    메인 메뉴의 옵션 UI에서 메인 메뉴 버튼 제거
    메인 메뉴에서 언어를 바꾼 다음에 컬렉션을 확인하면 언어가 바뀌지 않던 문제 해결

--- Assets/Scripts/UI/CollectionUI/CollectionSingleUI.cs
+++ Assets/Scripts/UI/CollectionUI/CollectionSingleUI.cs
@@ -2,32 +2,47 @@ using UnityEngine;
 
 public class CollectionSingleUI : UIFocusHandler
 {
+    private AbilityDiceSO _abilityDiceSO;
+
     public void Init(AbilityDiceSO abilityDiceSO)
     {
+        // AbilityDiceSO 저장
+        _abilityDiceSO = abilityDiceSO;
+
+        //다이스 스프라이트 리스트 가져오기
         var defaultDiceSpriteList = DataContainer.Instance.CurrentPlayerStat.diceSpriteListSO.spriteList;
 
-        string descriptionText = abilityDiceSO.IsUnlcoked() ? abilityDiceSO.GetDescriptionText() : abilityDiceSO.abilityUnlock.GetDescriptionText();
+        //해금 상태에 따른 색 지정
         Color color = abilityDiceSO.IsUnlcoked() ? Color.white : new Color(1f, 1f, 1f, 0.5f);
 
-        OnFocused += () =>
-        {
-            ToolTipUIEvents.TriggerOnToolTipShowRequested(
-                RectTransform,
-                Vector2.left,
-                abilityDiceSO.DiceName,
-                descriptionText,
-                ToolTipTag.AbilityDice,
-                abilityDiceSO.rarity
-            );
-        };
+        //이벤트 등록
+        OnFocused += HandleFocused;
+        OnUnfocused += HandleUnfocused;
 
-        OnUnfocused += () =>
-        {
-            ToolTipUIEvents.TriggerOnToolTipHideRequested(RectTransform);
-        };
-
+        //이미지 설정
         Image.sprite = defaultDiceSpriteList[abilityDiceSO.MaxDiceValue - 1];
         Image.material = abilityDiceSO.shaderDataSO.imageMaterial;
         Image.color = color;
+    }
+
+    // 포커스 되었을 때 툴팁 표시
+    private void HandleFocused()
+    {
+        string descriptionText = _abilityDiceSO.IsUnlcoked() ? _abilityDiceSO.GetDescriptionText() : _abilityDiceSO.abilityUnlock.GetDescriptionText();
+
+        ToolTipUIEvents.TriggerOnToolTipShowRequested(
+            RectTransform,
+            Vector2.left,
+            _abilityDiceSO.DiceName,
+            descriptionText,
+            ToolTipTag.AbilityDice,
+            _abilityDiceSO.rarity
+        );
+    }
+
+    // 포커스 해제 되었을 때 툴팁 숨기기
+    private void HandleUnfocused()
+    {
+        ToolTipUIEvents.TriggerOnToolTipHideRequested(RectTransform);
     }
 }
